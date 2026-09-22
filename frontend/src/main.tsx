ced1797eff09c8a7c10d9ff5a15437ec71909655
Matej Hurta <matejhurta11@gmail.com>
2026-09-22 18:26:03 +0200

    refactored css to ITCSS

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from '@mui/material/styles'
 import { theme } from './styles/muiTheme'
-import './styles/spotsteer.css'
+import './styles/index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
